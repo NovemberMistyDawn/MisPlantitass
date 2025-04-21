72075d798202843f3d8bf6aa0cc6474674681b75
mariacg <merycalvo98@hotmail.com>
2025-04-21 10:41:28 +0200

    primer commit

--- app/src/main/java/com/example/mis_plantitass/adapters/myplantsAdapter.kt
+++ app/src/main/java/com/example/mis_plantitass/adapters/myplantsAdapter.kt
@@ -3,8 +3,10 @@ package com.example.mis_plantitass.adapters
 import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView.Adapter
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.example.mis_plantitass.R
 import com.example.mis_plantitass.activities.MyPlantsActivity
 import com.example.mis_plantitass.activities.MyPlantDetailActivity
 import com.example.mis_plantitass.data.MyPlant
@@ -21,15 +23,22 @@ class myplantsAdapter(
         return MyPlantViewHolder(binding)
     }
 
-
     override fun getItemCount(): Int = items.size
 
     override fun onBindViewHolder(holder: MyPlantViewHolder, position: Int) {
         val myplant = items[position]
         holder.render(myplant)
+
+        // Cambiar el fondo del LinearLayout dentro del CardView dependiendo del estado de la planta
+        if (myplant.regada) {
+            holder.binding.itemLayout.setBackgroundColor(ContextCompat.getColor(holder.itemView.context, android.R.color.white)) // Fondo blanco
+        } else {
+            holder.binding.itemLayout.setBackgroundColor(ContextCompat.getColor(holder.itemView.context, R.color.lightBlue)) // Fondo azul (lightBlue)
+        }
+
         // En lugar de crear un Intent aquí, ya que tienes un onClick en MyPlantsActivity
         holder.itemView.setOnClickListener {
-            onClick(position)  // Llamamos al metodo editPlant en MyPlantsActivity
+            onClick(position)  // Llamamos al método editPlant en MyPlantsActivity
         }
 
         holder.binding.deleteButton.setOnClickListener {
